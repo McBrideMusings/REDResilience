5a5d341264a8b9faea8ff884a4ac4779f0c79fa1
Pierce McBride <piercermcbride@gmail.com>
2018-03-07 15:52:15 -0500

    Small Edits

--- client/src/App.js
+++ client/src/App.js
@@ -4,32 +4,26 @@ import Header from './components/header/header'
 import './App.css'; // Also is import a JS thing? Or react?
 
 class App extends Component {
-  state = {test: null} // State as far as I can tell is just an object we can store whatever in that forces a rerender when set with setState
+  state = {
+    screen: 0,
+    test: null
+  }
 
   componentDidMount() {
     fetch('/', {
-      method: 'POST' // I think I should be using the header and body sections but it worked without it for now
+      method: 'POST' 
     })
     .then(res => res.json())
     .then(data => {
       console.log(data);
       this.setState({test: data});
     });
-    /*
-    fetch('/data', {
-      method: 'POST' // I think I should be using the header and body sections but it worked without it for now
-    })
-    .then(res => console.log(res)); 
-    */
   }
 
   render() {
     return (
       <div className="App"> 
         <Header State="This"></Header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         {this.checkState().map(sheet => 
           <div key={sheet.id}>{sheet.fulladdress}</div>
         )}
