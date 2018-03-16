e57883e159ef2c6afa4b8025aa5fc4482e3dc96e
Pierce McBride <piercermcbride@gmail.com>
2018-03-16 02:06:01 -0400

    Added Photo Component

---
+++ client/src/pages/upload/Upload.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Upload extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col s12 flex flex-column">
+          loading data....
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Upload;
