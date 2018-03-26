573fa053562f8630c5a8ca7d5909f45e598723f5
Pierce McBride <piercermcbride@gmail.com>
2018-03-25 20:10:04 -0400

    Photo upload feature working at /upload

--- client/src/pages/upload/Upload.js
+++ client/src/pages/upload/Upload.js
@@ -1,19 +1,81 @@
 import React, { Component } from 'react';
 
+import UploadPics from './UploadPics';
+import ViewPics from './ViewPics';
+
 class Upload extends Component {
+  constructor() {
+    super();
+    this.state = {
+      files: []
+    };
+  }
+
+  handler = data => {
+    console.log("thuis");
+    console.log(data);
+    this.setState({
+      files: data
+    })
+  }
+
+  onClick = (e) => {
+    this.setState({ uploading: true });
+  }
+
+  render() {
+    if (this.state.files.length === 0){
+      return (
+        <div className="container">
+          <div className="row">
+            <UploadPics handler={this.handler}></UploadPics>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="container">
+          <div className="row">
+            <ViewPics files={this.state.files}></ViewPics>
+          </div>
+        </div>
+      );
+    }
+
+  }
+  /*
   constructor(props) {
     super(props);
     this.state = {
-      imageURL: '',
       uploadedImgs: []
     };
-    this.handleUploadImage = this.handleUploadImage.bind(this);
   }
-  handleUploadImage(event) {
-    event.preventDefault();
+  onChange = (e) => {
+    const state = this.state;
+
+    switch (e.target.name) {
+      case 'selectedFile':
+        state.selectedFile = e.target.files[0];
+        break;
+      default:
+        state[e.target.name] = e.target.value;
+    }
+
+    this.setState(state);
+  }
+
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    // get our form data out of state
     const data = new FormData();
     data.append('file', this.uploadInput.files[0]);
     data.append('filename', this.uploadInput.files[0].name);
+  
+    axios.post('/upload', data)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+    
     fetch('/upload', { // Your POST endpoint
       method: 'POST',
       body: data,
@@ -24,33 +86,6 @@ class Upload extends Component {
     }).catch( error => {
       console.log(error);
     });
-    /*
-    fetch('/upload', { // Your POST endpoint
-      method: 'POST',
-      body: data,
-    }).then( 
-      response => response.json() // if the response is a JSON object
-    ).then( 
-      success => console.log(success) // Handle the success response object
-    ).catch(
-      error => console.log(error) // Handle the error response object
-    );
-    */
-    /*
-    fetch('/upload', { // Your POST endpoint
-      method: 'POST',
-      body: data,
-    }).then( (response) => function() {
-      console.log("data back");
-      response.json() // if the response is a JSON object
-    }).then( (success) => function () {
-      console.log(success);
-      //this.setState({ imageURL: `http://localhost:8000/${body.file}` });
-      //console.log(this.state);
-    }).catch(
-      error => console.log(error) // Handle the error response object
-    );
-    */
   }
   render() {
     console.log("Rendering");
@@ -58,23 +93,20 @@ class Upload extends Component {
       <div className="container">
         <div className="row">
           <div className="col s12 flex flex-column">
-            <form onSubmit={this.handleUploadImage}>
+            <form onSubmit={this.onSubmit}>
               <div>
                 <input ref={(ref) => { this.uploadInput = ref; }} type="file"></input>
-              </div>
-              <div>
-                List of Uploaded Images
               </div>
               <div>
                 <button type="submit">Upload</button>
               </div>
             </form>
-            <img src={this.state.imageURL} alt="img" />
           </div>
         </div>
       </div>
     );
   }
+  */
 }
 
 export default Upload;
--- server.js
+++ server.js
@@ -1,42 +1,89 @@
 "use strict";
+// libraries
+const port              = process.env.PORT || 3001;
 const express           = require("express");
 const bodyParser        = require('body-parser');
-const fileUpload        = require('express-fileupload');
+const multer            = require('multer');
+const uuidv4            = require('uuid/v4');
 const cors              = require('cors');
 const fs 	              = require("fs");
 const path              = require('path');
 const GoogleSpreadsheet = require('google-spreadsheet');
 
-const doc               = new GoogleSpreadsheet('1AEvI_VAcKss93_angVXQowdvnjL8u0diIjLsO3vnlJs');
-const data              = require(__dirname + "/data.json");
-const creds             = require('./master-creds.json');
+
+// config
+const maxFileSize = 10000000000; // Might be total across all uploaded files
+const maxNumFiles = 10;
+
+
+// setup
 var sheet;
+const creds = require('./master-creds.json');
+const data = require(__dirname + "/data.json");
+const doc = new GoogleSpreadsheet('1AEvI_VAcKss93_angVXQowdvnjL8u0diIjLsO3vnlJs');
+
+// configure storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    /*
+      Files will be saved in the 'uploads' directory. Make
+      sure this directory already exists!
+    */
+    cb(null, `${__dirname}/public/uploads/`);
+  },
+  filename: (req, file, cb) => {
+    /*
+      uuidv4() will generate a random ID that we'll use for the
+      new filename. We use path.extname() to get
+      the extension from the original file name and add that to the new
+      generated ID. These combined will create the file name used
+      to save the file on the server and will be available as
+      req.file.pathname in the router handler.
+    */
+    const newFilename = `${uuidv4()}${path.extname(file.originalname)}`;
+    cb(null, newFilename);
+  },
+});
+// create the multer instance that will be used to upload/save the file
+const upload = multer({ 
+  storage: storage,
+  limits: { fileSize: maxFileSize }
+});
 
 const app = express();
-
-app.set("port", process.env.PORT || 3001);
-app.use(cors());
+app.use(express.static('./public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(fileUpload());
-// Express only serves static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-  res.send("test");
+
+// app 
+app.post('/upload', (req, res) => {
+  upload.array("userPhoto", maxNumFiles)(req,res,function(err) {
+    /*
+      We now have a new req.file object here. At this point the file has been saved
+      and the req.file.filename value will be the name returned by the
+      filename() function defined in the diskStorage configuration. Other form fields
+      are available here in req.body.
+    */
+    console.log(req.files);
+    if(err) {
+      res.status(500).send({ error: err.code });
+      return res.end();
+    }
+    // res.end("File is uploaded");
+    let fileNames = [];
+    for (let i = 0; i < req.files.length; i++) {
+      // This is possibly what we want in production
+      // fileNames[i] = req.files[i].destination+req.files[i].filename;
+      fileNames[i] = req.files[i].filename;
+    }
+    res.json({files: fileNames});
+    //res.end("File is uploaded");
+  });
 });
 
-app.get("/test", function (req, res) {
-  //res.sendFile(filebuffer); // I'd like to figure out the fs system but it never worked for me
-  res.json(data);
-  //res.json(filebuffer);
-});
-
-app.post("/", (req, res) => {
-  res.json(data);
-});
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+/*
 
 app.get("/data", (req, res) => {
   setAuth(function(){
@@ -91,3 +138,4 @@ function setAuth(callback) {
 function formatFullAddress(streetNumber,streetName,city,state,zip) {
   return streetNumber+" "+streetName+" "+city+", "+state+" "+zip;
 }
+*/
